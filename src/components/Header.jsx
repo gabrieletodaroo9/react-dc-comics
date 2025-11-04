0d96a8799279e660bd001d3a7998ae861e57546d
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-11-04 14:14:21 +0100

    add base layout of header

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+import LogoDc from '../assets/img/dc-logo.png'
+
+export default function Header() {
+
+    return (
+        <>
+            <header>
+                <nav className="container d-flex justify-content-between align-items-center ">
+                    <div>
+                        <img src={LogoDc} alt="logo-dc" className='w-50' />
+                    </div>
+                    <ul className='list-unstyled d-flex gap-4'>
+                        <li className=''>
+                            <a href="">CHARACTERS</a>
+                        </li>
+                        <li className=''>
+                            <a href="">COMICS</a>
+                        </li>
+                        <li className=''>
+                            <a href="">MOVIES</a>
+                        </li>
+                        <li className=''>
+                            <a href="">TV</a>
+                        </li>
+                        <li className=''>
+                            <a href="">GAMES</a>
+                        </li>
+                        <li className=''>
+                            <a href="">COLLECTIBLES</a>
+                        </li>
+                        <li className=''>
+                            <a href="">VIDEOS</a>
+                        </li>
+                        <li className=''>
+                            <a href="">FANS</a>
+                        </li>
+                        <li className=''>
+                            <a href="">NEWS</a>
+                        </li>
+                        <li className=''>
+                            <a href="">SHOP</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+
+        </>
+    )
+
+}
