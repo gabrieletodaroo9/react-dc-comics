52e848753ff863f53f1e9d08da917c8fe8382d65
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-11-05 16:38:07 +0100

    add new component for social footer bar

---
+++ src/components/FooterSocial.jsx
@@ -0,0 +1,39 @@
+import FacebookImg from '../assets/img/footer-facebook.png'
+import PeriscopeImg from '../assets/img/footer-periscope.png'
+import PinterestImg from '../assets/img/footer-pinterest.png'
+import TwitterImg from '../assets/img/footer-twitter.png'
+import YouTubeImg from '../assets/img/footer-youtube.png'
+
+export default function FooterSocial() {
+    return (
+        <>
+            <div className='footer-social bg-dark'>
+                <div className="container text-white d-flex justify-content-between">
+                    <a className='btn btn dark' href="">
+                        SIGN UP NOW!
+                    </a>
+                    <div className='social-icons d-flex gap-2 gap-sm-3 align-items-center'>
+                        <span className='follow-span d-none d-md-block'>
+                            FOLLOW US
+                        </span>
+                        <a href="">
+                            <img src={FacebookImg} alt="facebook-icon" />
+                        </a>
+                        <a href="">
+                            <img src={PeriscopeImg} alt="periscope-icon" />
+                        </a>
+                        <a href="">
+                            <img src={PinterestImg} alt="pinterest-icon" />
+                        </a>
+                        <a href="">
+                            <img src={TwitterImg} alt="twitter-icon" />
+                        </a>
+                        <a href="">
+                            <img src={YouTubeImg} alt="youtube-icon" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
